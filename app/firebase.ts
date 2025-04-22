38acb4f7a842a3c608eb35e8d56651926a3bc141
NOOB-3301 <sepic466@gmail.com>
2025-04-22 23:50:52 +0530

    feat: update Firebase configuration to use environment variables and modify app metadata

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBOWn7TzOaPqyl5Y5C3DfTnFnJ8wgOB1IQ",
-  authDomain: "third-firestore.firebaseapp.com",
-  databaseURL: "https://third-firestore-default-rtdb.firebaseio.com",
-  projectId: "third-firestore",
-  storageBucket: "third-firestore.firebasestorage.app",
-  messagingSenderId: "792530130881",
-  appId: "1:792530130881:web:88cef8189a17d0a65ec140"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!
 };
 
-// Initialize Firebase
 const f_app = initializeApp(firebaseConfig);
-const db = getFirestore(f_app)
-export {db}
+const db = getFirestore(f_app);
+export { db };
